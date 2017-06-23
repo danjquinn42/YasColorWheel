29299eb7c651a2aaa95c37235fb90c6f5ece53fe
danjquinn42 <danjquinn42@gmail.com>
2017-06-23 02:13:20 -0400

    refine degrees tests

--- spec/Degrees_Spec.js
+++ spec/Degrees_Spec.js
@@ -1,23 +1,23 @@
-const Degree = require("../scripts/math/degrees.js");
-const DegreeEquality = function(first, second) {
-  if(typeof first === "Degree" && typeof second === "Degree") {
+const Degrees = require("../scripts/math/degrees.js");
+const DegreesEquality = function(first, second) {
+  if(typeof first === "Degrees" && typeof second === "Degrees") {
     return first.equals(second);
   }
 };
 
-describe("Degree", function() {
+describe("Degrees", function() {
 
   beforeEach(function() {
-    degreeEighty = new Degree(80);
-    degreeThreeHundred = new Degree(300);
+    degreeEighty = new Degrees(80);
+    degreeThreeHundred = new Degrees(300);
   });
 
   it("should set the modulo of numbers over 360", function() {
-    expect(new Degree(370)).toEqual(new Degree(10));
+    expect(new Degrees(370)).toEqual(new Degrees(10));
   });
 
   it("should set the modulo of negative numbers", function() {
-    expect(new Degree(-60)).toEqual(degreeThreeHundred);
+    expect(new Degrees(-60)).toEqual(degreeThreeHundred);
   });
 
   describe("toRadians", function() {
@@ -28,14 +28,14 @@ describe("Degree", function() {
 
   describe("minus", function() {
     it("should return the difference of two degrees objects", function() {
-      expect(degreeThreeHundred.minus(degreeEighty)).toEqual(new Degree(220))
+      expect(degreeThreeHundred.minus(degreeEighty)).toEqual(new Degrees(220))
     });
   });
 
   describe("negated", function() {
     it("should be 180˚ differnet from the original value", function(){
       expect(degreeEighty.negated())
-        .toEqual((new Degree(360)).minus(degreeEighty));
+        .toEqual((new Degrees(360)).minus(degreeEighty));
     });
   });
 
