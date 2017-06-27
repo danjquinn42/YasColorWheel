da16b0d11446da7762352a7d8c7d78c10c95adc3
danjquinn42 <danjquinn42@gmail.com>
2017-06-27 12:38:31 -0400

    Remove beforeEach

--- spec/Degrees_Spec.js
+++ spec/Degrees_Spec.js
@@ -8,41 +8,26 @@ const DegreesEquality = function(first, second) {
 
 describe("Degrees", function() {
 
-  beforeEach(function() {
-    degreeEighty = new Degrees(80);
-    degreeThreeHundred = new Degrees(300);
+  const degreeEighty = new Degrees(80);
+  const degreeThreeHundred = new Degrees(300);
 
-    it("should set the modulo of numbers over 360", function() {
-      expect(new Degrees(370)).toEqual(new Degrees(10));
+  it("should set the modulo of numbers over 360", function() {
+    expect(new Degrees(370)).toEqual(new Degrees(10));
+  });
+
+  it("should set the modulo of negative numbers", function() {
+    expect(new Degrees(-60)).toEqual(degreeThreeHundred);
+  });
+
+  describe("toRadians", function() {
+    it("should return a Radians object of correct value", function() {
+      expect(degreeEighty.toRadians().value).toBeCloseTo(1.39626, 3);
     });
+  });
 
-    it("should set the modulo of negative numbers", function() {
-      expect(new Degrees(-60)).toEqual(degreeThreeHundred);
-    });
-
-    describe("toRadians", function() {
-      it("should return a Radians object of correct value", function() {
-        expect(degreeEighty.toRadians().value).toBeCloseTo(1.39626, 3);
-      });
-    });
-
-    describe("minus", function() {
-      it("should return the difference of two degrees objects", function() {
-        expect(degreeThreeHundred.minus(degreeEighty)).toEqual(new Degrees(220))
-      });
-    });
-
-    describe("negated", function() {
-      it("should be 180˚ differnet from the original value", function() {
-        expect(degreeEighty.negated())
-          .toEqual((new Degrees(360)).minus(degreeEighty));
-      });
-    });
-
-    describe("toString", function() {
-      it("should return a stringified degree with ˚ notation", function() {
-        expect(degreeEighty.toString()).toEqual("80˚");
-      });
+  describe("toString", function() {
+    it("should return a stringified degree with ˚ notation", function() {
+      expect(degreeEighty.toString()).toEqual("80˚");
     });
   });
 });
